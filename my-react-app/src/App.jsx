7b8a92ec4f803b1d43bad2ca4aea958302d53684
Maricruz1900 <utm22030695@utma.edu.mx>
2024-10-16 09:41:09 -0600

    feat: initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './components/Login.jsx';
+import './index.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // Para incluir todos los archivos JS y JSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
